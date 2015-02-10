bb2cf52e9a24094cbb0dd52bca1a3412eb36e9b4
Peter Uhe <Peter.Uhe@ouce.ox.ac.uk>
2015-02-10 16:19:37 +0000

    update to scripts

--- wah_eu_2014/calculate_returntimes.py
+++ wah_eu_2014/calculate_returntimes.py
@@ -3,7 +3,7 @@ from sort_umids import sort_tasknames
 import numpy as np
 from netcdf_file import netcdf_file
 import matplotlib.pyplot as plt
-from extract_zips import create_netcdf
+
 # Takes 4 seasonal bias files and calculates a yearly average
 def ave_bias(bias_files):
 	data=np.ma.zeros([108,114])
@@ -35,16 +35,32 @@ def remap_N96_to_rotated(N96_file,rot_template):
 	dataout=np.zeros([108,114])
 	for i in range(114):
 		for j in range(108):
-#			print j,i
-			index_lat=int(((g_lat[j,i]+90)/1.25)+0.5)%145
+			# Calculate nearest neighbor indices 
+			index_lat=int(((g_lat[j,i]+90)/1.25)+0.5)
 			index_lon=int((g_lon[j,i]/1.875)+0.5)%192
-#			print index_lat,index_lon
 			dataout[j,i]=N96_data[index_lat,index_lon]
 	dataout=np.ma.masked_values(dataout,0.0)
-	return dataout			
+	return dataout		
+	
+def remap_p5deg_to_rotated(p5deg_file,rot_template):
+	f_rot=netcdf_file(rot_template,'r')
+	g_lat=f_rot.variables['global_latitude0'][4:-7,4:-4]
+	g_lon=f_rot.variables['global_longitude0'][4:-7,4:-4]
+	p5deg_nc=netcdf_file(p5deg_file,'r').variables['tasmean']
+	p5deg_data=p5deg_nc[0,:]
+	print p5deg_data.shape
+	dataout=np.zeros([108,114])
+	for i in range(114):
+		for j in range(108):
+			# Calculate nearest neighbor indices 
+			index_lat=int(((g_lat[j,i]+89.75)/0.5)+0.5) 
+			index_lon=int((g_lon[j,i]/0.5)+0.5)%720
+			dataout[j,i]=p5deg_data[index_lat,index_lon]
+	dataout=np.ma.masked_values(dataout,p5deg_nc._FillValue)
+	return dataout
 
 
-# Create netcdf file, removing the 'sponge layer' at the edge of the domain
+# Create netcdf file on rotated grid, removing the 'sponge layer' at the edge of the domain
 def create_netcdf_stripped(template,data,outname,lat_start=4,lat_end=7,lon_start=4,lon_end=4):
 	# create outfile object
 	outfile=netcdf_file(outname,'w')
@@ -53,20 +69,23 @@ def create_netcdf_stripped(template,data,outname,lat_start=4,lat_end=7,lon_start
 	temp=template.variables['tas']
 	for dim in temp.dimensions:
 		if dim=='time1': 
-			leng=0
+			leng=1
 			outfile.createDimension(dim,leng)
 			outfile.createVariable(dim,'f',(dim,))
 			outfile.variables[dim][:]=template.variables[dim][:]
+			outfile.variables[dim].__setattr__('axis','T')
 		elif dim[:3]=='lat': 
 			leng=int(template.dimensions[dim])-lat_start-lat_end
 			outfile.createDimension(dim,leng)
 			outfile.createVariable(dim,'f',(dim,))
 			outfile.variables[dim][:]=template.variables[dim][lat_start:-lat_end]
+			outfile.variables[dim].__setattr__('axis','Y')
 		elif dim[:3]=='lon' : 
 			leng=int(template.dimensions[dim])-lon_start-lon_end
 			outfile.createDimension(dim,leng)
 			outfile.createVariable(dim,'f',(dim,))
 			outfile.variables[dim][:]=template.variables[dim][lon_start:-lon_end]
+			outfile.variables[dim].__setattr__('axis','X')
 		else: 
 			leng=int(template.dimensions[dim])
 			outfile.createDimension(dim,leng)
@@ -75,7 +94,9 @@ def create_netcdf_stripped(template,data,outname,lat_start=4,lat_end=7,lon_start
 			
 		for att in template.variables[dim]._attributes:
 			outfile.variables[dim].__setattr__(att,template.variables[dim].__getattribute__(att))
-	
+		
+		if dim=='time1':	
+			outfile.variables[dim].__setattr__('calendar','gregorian') # Hack so Karsten can read these files in grads
 	# Rotated pole variable
 	outfile.createVariable('rotated_pole0','c',())
 	for att in template.variables['rotated_pole0']._attributes:
@@ -83,49 +104,68 @@ def create_netcdf_stripped(template,data,outname,lat_start=4,lat_end=7,lon_start
 	
 	# Global latitude
 	outfile.createVariable('global_latitude0','f',template.variables['global_latitude0'].dimensions)
-	outfile.variables['global_latitude0'][:]=template.variables['global_latitude0'][lat_start:-lat_end,lon_start-lon_end]
+	outfile.variables['global_latitude0'][:]=template.variables['global_latitude0'][lat_start:-lat_end,lon_start:-lon_end]
 	for att in template.variables['global_latitude0']._attributes:
 			outfile.variables['global_latitude0'].__setattr__(att,template.variables['global_latitude0'].__getattribute__(att))
 			
 	# Global longitude
 	outfile.createVariable('global_longitude0','f',template.variables['global_longitude0'].dimensions)
-	outfile.variables['global_longitude0'][:]=template.variables['global_longitude0'][lat_start:-lat_end,lon_start-lon_end]
+	outfile.variables['global_longitude0'][:]=template.variables['global_longitude0'][lat_start:-lat_end,lon_start:-lon_end]
 	for att in template.variables['global_longitude0']._attributes:
 			outfile.variables['global_longitude0'].__setattr__(att,template.variables['global_longitude0'].__getattribute__(att))
 	
 	# Create data variable (named tas)
 	outfile.createVariable('tas','f',temp.dimensions)
-	outfile.variables['tas'][:]=data
+	outfile.variables['tas'].assignValue(data.filled(temp._FillValue))
 	for att in temp._attributes:
 		outfile.variables['tas'].__setattr__(att,temp.__getattribute__(att))
 	
-	outfile.flush()
 	outfile.close()
 
 
 if __name__=='__main__':
 	infiles='/ouce-home/staff/cenv0437/data/batch_100/hadam3p_eu_*_tasmean.nc'
 	bias_files='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/CRUT4/EU_???_temp-cruts_0.44_mean.nc'
-	bias = ave_bias(bias_files)
+	bias = ave_bias(bias_files)[:,:-10]
 	(historical_files,natural_files)=sort_tasknames(infiles)
-	
+
 	# Load historical data into single array and bias correct
 	print len(historical_files)
-	hist_raw=load_ensemble(historical_files)
+	hist_raw=np.ma.array(load_ensemble(historical_files))[:,:,:-10]
+	# Apply mask (only need to do this when not bias correcting)
+	for i in range(len(historical_files)):
+	        hist_raw[i,:]=np.ma.masked_where(bias.mask,hist_raw[i,:])
 	historical_data=hist_raw-bias
 	
 	# Load natural simulation data into single array and bias correct
 	print len(natural_files)
-	natural_data=load_ensemble(natural_files)-bias
+	natural_raw=np.ma.array(load_ensemble(natural_files))[:,:,:-10]
+	# Apply mask (only need to do this when not bias correcting)
+	for i in range(len(natural_files)):
+	        natural_raw[i,:]=np.ma.masked_where(bias.mask,natural_raw[i,:])
+	natural_data=natural_raw-bias
 	
-	
-	# TODO Get real OBS!!!
+	# CRUTEM observational set
 	obs_N96='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/final/CRU_T2m_dec-nov_N96_interp_lsm.nc'
+
+	# Obs using GFS analysis
+#	obs_p5deg='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/HALF_final/GFS_dec13-nov14_biascorrected.nc'
+
+	# Obs using higher resolution cru.ts climatology
+	obs_p5deg='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/HALF_final/CRU_TS_dec13-nov14_crut4anomalies.nc'
+
+	# Regrid obs to rotated regional grid
 	rot_template='/ouce-home/staff/cenv0437/data/batch_100/hadam3p_eu_z4ao_2013_1_009238311_0_tasmean.nc'
-	obs=remap_N96_to_rotated(obs_N96,rot_template)
-	
-	# Write out calculated obs on rotated grid
-	create_netcdf_stripped(netcdf_file(rot_template),obs,'CRU_T2m_dec-nov_regional.nc')
+#	obs=remap_N96_to_rotated(obs_N96,rot_template)
+	obs=remap_p5deg_to_rotated(obs_p5deg,rot_template)[:,:-10]
+
+	oldfiles=glob.glob('/ouce-home/staff/cenv0437/data/batch_43/hadam3p_eu_*_tasmean.nc')
+	print len(oldfiles)
+        old_raw=load_ensemble(oldfiles)[:,:,:-10]
+        old_data=old_raw-bias
+
+#################################################
+# Calculate diagnostics	
 	
 	# Count number of model runs which are above the obs
 	condition=historical_data>obs
@@ -133,32 +173,122 @@ if __name__=='__main__':
 	
 	condition=natural_data>obs
 	count_nat=condition.sum(0)/(len(natural_files)*1.0)
+	
+	chist2=(historical_data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(historical_files)*1.0)
+	cnat2=(natural_data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(natural_files)*1.0)
 
-# Non vectorised way of calculating count 
+	chist3=(hist_raw.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(historical_files)*1.0)
+	cnat3=(natural_raw.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(natural_files)*1.0)
+
+# Old ensemble of historical simulations between 2000-2008
+	cold=(old_data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(oldfiles)*1.0)
+
+# Region to look at germany:
+	hmean=hist_raw[:,50:60,10:50].mean(1).mean(1)
+	nmean=natural_raw[:,50:60,10:50].mean(1).mean(1)	
+	obsmean=obs[50:60,10:50].mean(0).mean(0)
+	chist4=(hmean>obsmean).sum(0)/(len(historical_files)*1.0)
+	cnat4=(nmean>obsmean).sum(0)/(len(natural_files)*1.0)
+
+	print 'return times: all forcings=',1/chist2,'natural forcings=',1/cnat2,'2000-2008=',1/cold
+	print 'far',1-(cnat2/chist2)
+	print 'change in risk', (chist2-cnat2)/cnat2
+	
+	print 'Without bias correction:'
+	print 'return times: all forcings=',1/chist3,'natural forcings=',1/cnat3
+	print 'far',1-(cnat3/chist3)
+	print 'change in risk', (chist3-cnat3)/cnat3
+	
+	print 'Germany:'
+	print 'return times: all forcings=',1/chist4,'natural forcings=',1/cnat4
+	print 'far',1-(cnat4/chist4)
+	print 'change in risk', (chist4-cnat4)/cnat4
+	
+	# Non vectorised way of calculating count 
 #	count = np.zeros([119,122])
 #	for i in range(122):
 #		for j in range(119):
 #			count[j,i] = (historical_data[:,j,i]>obs).sum()/(len(historical_files)*1.0)
-
-# Plot stuff
-	plt.figure(1)
-#	plt.contourf((count_hist[::-1,:]))
-	plt.contourf((historical_data.mean(0)-obs)[::-1,:],20)
-	plt.colorbar()
 	
-	plt.figure(2)
-#	plt.contourf((count_nat[::-1,:]))
-	plt.contourf(bias[::-1,:],20)
+	# Change in likelihood
+	change=(count_hist-count_nat)/(count_nat+1e-5)
+	
+	# Anomaly between obs and ensemble mean of historical simulations
+	anom=obs-(historical_data.mean(0))
+
+	# Anomaly between obs and ensemble mean of nat simulations
+	anom_nat=obs-(natural_data.mean(0))
+
+	# FAR
+	far=1.0-((count_nat)/(count_hist+1e-5))
+	far=far*((count_hist!=0.0)*1.0) # Set to zero where count_hist==0
+
+####################################################
+	# Write out data to netcdf
+#	create_netcdf_stripped(netcdf_file(rot_template),obs,'CRU_T2m_dec-nov_regional.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),hist_raw,'hist_raw.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),historical_data,'hist_corrected.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),natural_raw,'natural_raw.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),natural_data,'natural_corrected.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),anom,'anom_corrected.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),count_hist,'prob_historical_corrected.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),count_nat,'prob_natural_corrected.nc')
+#	create_netcdf_stripped(netcdf_file(rot_template),far,'far_corrected.nc')
+
+######################################################
+# Plot stuff
+
+#First get rid of annoying Mac hidden files
+	delfiles=glob.glob('._*.png')
+	for f in delfiles:
+		os.remove(f)
+		
+	plt.figure(1)
+	plt.contourf((count_hist[::-1,:]),np.arange(0,1.05,.05),extend='both')
 	plt.colorbar()
+	plt.title('Probability of temperature being above 2014 temp = '+str(count_hist.mean()))
+	plt.savefig('figure1.png')
+		
+	plt.figure(2)
+	plt.contourf((count_nat[::-1,:]),np.arange(0,0.25,.01),extend='both')
+	plt.colorbar()
+	plt.title('Probability of temperature being above 2014 temp \nwith natural forcings only = '+str(count_nat.mean()))
+	plt.savefig('figure2.png')
 	
 	plt.figure(3)
+	plt.title('Observational dataset of Temperature for 2014 = '+str(obs.mean()))
 	plt.contourf(obs[::-1,:],20)
 	plt.colorbar()
-	plt.show()	
+	plt.savefig('figure3.png')
 	
-#	far=1.0-(count_nat/count_hist)
-#	plt.figure(3)
-#	plt.contourf(far[::-1,:])
-#	plt.colorbar()
-#	plt.show()
+	plt.figure(4)
+	plt.title('2014 Anomaly (Observations - Ensemble mean) = '+str(anom.mean()))
+	plt.contourf(anom[::-1,:],20)
+	plt.colorbar()
+	plt.savefig('figure4.png')
 	
+	plt.figure(5)
+	plt.title('Climatalogical bias used to bias correct model = '+str(bias.mean()))
+	plt.contourf(bias[::-1,:],20)
+	plt.colorbar()
+	plt.savefig('figure5.png')
+	
+	plt.figure(6)
+	plt.title('FAR = '+str(far.mean()))
+	plt.contourf(far[::-1,:],np.arange(0,1.05,.05),extend='both')
+	plt.colorbar()
+	plt.savefig('figure6.png')	
+	
+	plt.figure(7)
+	plt.title('Change in likelihood of temp greater than 2014 ='+str(change.mean()))
+	plt.contourf(change[::-1,:],np.arange(0,22,2),extend='both')
+	plt.colorbar()
+	plt.savefig('figure7.png')	
+	
+	plt.figure(8)
+	plt.title('2014 Anomaly (Observations - Nat Ensemble mean) = '+str(anom_nat.mean()))
+	plt.contourf(anom_nat[::-1,:],20)
+	plt.colorbar()
+	plt.savefig('figure8.png')	
+		
+	#plt.show()
--- wah_eu_2014/extract_zips.py
+++ wah_eu_2014/extract_zips.py
@@ -2,99 +2,66 @@ import os,shutil,zipfile,glob
 import numpy as np
 #from scipy.io.netcdf import netcdf_file
 from netcdf_file import netcdf_file
-file_map={1:'l3dec',2:'l4jan',3:'l4feb',4:'l4mar',5:'l4apr',6:'l4may',7:'l4jun',8:'l4jul',9:'l4aug',10:'l4sep',11:'l4oct',12:'l4nov'}
 
-def yearly_mean(ncfiles,outfile):
-	print ncfiles[0]
+
+
+# Function to calculate yearly mean from monthly means
+def yearly_mean(ncfiles):
 	# Open first nc file to get dimensions
 	tmp=netcdf_file(ncfiles[0],'r')
-	nt,nl,nlat,nlon=tmp.variables['field16'].shape
-	meantemp=np.zeros([1,1,nlat,nlon])
+	meantemp=np.zeros(tmp.variables['field16'].shape)
+	tmp.close()
+
 	i=0
-	# Loop over list of tasks and copy to scratch space
+	# Loop over list of months 
 	for filename in ncfiles:
 		ncfile=netcdf_file(filename,'r')
 		data=ncfile.variables['field16'][:]
+		
+		# Data consistency checking (for Temperature only!!!)
 		if data.min()<170.0 or data.max()>350.0:
-			return False
+			raise Exception('Data outside reasonable bounds')
 		meantemp=meantemp+data
 		i=i+1
 	if i!=12: # Need yearly mean
-		return False
+		raise Exception('Less than a years data is present')
 	else:
-		meantemp=meantemp/12
-		create_netcdf(tmp,meantemp,outfile)
+		return meantemp/12
  
+#function to return zip name 
+def nc_filename(umid,year,zip_num,pp_code):
+	mon_map={1:'dec',2:'jan',3:'feb',4:'mar',5:'apr',6:'may',7:'jun',8:'jul',9:'aug',10:'sep',11:'oct',12:'nov'}
+	if zip_num>1: year=year+1
+	decade=(int(year)-1800)/10
+	year_code=str(year%10)
+	if decade<10:   dec_code=str(decade) #decade is 0 to 9
+	else:           dec_code=chr(decade-10+ord('a')) #decade needs to be converted to char
+	
+	return umid+pp_code+dec_code+year_code+mon_map[zip_num]+'.nc'
+ 
+ # Extract one ncfile from each monthly zip and return list
 def extract_zips(taskdir,outpath):
+#	file_map={1:'l3dec',2:'l4jan',3:'l4feb',4:'l4mar',5:'l4apr',6:'l4may',7:'l4jun',8:'l4jul',9:'l4aug',10:'l4sep',11:'l4oct',12:'l4nov'}
+#	mon_map={'jan':1,'feb':2,'mar':3,'apr':4,'may':5,'jun':6,'jul':7,'aug':8,'sep':9,'oct':10,'nov':11,'dec':12}	
+
 	# Make tmp folder
 	tmpdir=os.path.join(outpath,'tmp')
 	if not os.path.exists(tmpdir):
 		os.makedirs(tmpdir)		
 	taskname = taskdir.split('/')[-1]
-	umid=taskname.split('_')[2]
+	[umid,year]=taskname.split('_')[2:4]
 	extracted=[]
 	for i in range(1,13):
 		zipname=os.path.join(taskdir,taskname+'_'+str(i)+'.zip')
 		if os.path.exists(zipname):
 			zip=zipfile.ZipFile(zipname,'r')
 			# Just extract the monthly means from regional model
-			ncfile=umid+'ga.pe'+file_map[i]+'.nc'
+#			ncfile=umid+'ga.pe'+file_map[i]+'.nc'
+			ncfile=nc_filename(umid,int(year),i,'ga.pe')
 			zip.extract(ncfile,tmpdir) 
-			print zipname,i
 			extracted.append(os.path.join(tmpdir,ncfile))
 		# Only return if all of the zips are present
-		else: return []
-	return extracted
-	
-def create_netcdf(template,data,outname):
-	# create outfile object
-	outfile=netcdf_file(outname,'w')
-	
-	# Create dimimport os,shutil,zipfile,glob
-import numpy as np
-#from scipy.io.netcdf import netcdf_file
-from netcdf_file import netcdf_file
-file_map={1:'l3dec',2:'l4jan',3:'l4feb',4:'l4mar',5:'l4apr',6:'l4may',7:'l4jun',8:'l4jul',9:'l4aug',10:'l4sep',11:'l4oct',12:'l4nov'}
-
-def yearly_mean(ncfiles,outfile):
-	# Open first nc file to get dimensions
-	tmp=netcdf_file(ncfiles[0],'r')
-	nt,nl,nlat,nlon=tmp.variables['field16'].shape
-	meantemp=np.zeros([1,1,nlat,nlon])
-	i=0
-	# Loop over list of tasks and copy to scratch space
-	for filename in ncfiles:
-		ncfile=netcdf_file(filename,'r')
-		data=ncfile.variables['field16'][:]
-		if data.min()<170.0 or data.max()>350.0:
-			return False
-		meantemp=meantemp+data
-		i=i+1
-	if i!=12: # Need yearly mean
-		return False
-	else:
-		meantemp=meantemp/12
-		create_netcdf(tmp,meantemp,outfile)
-		print outfile
- 
-def extract_zips(taskdir,outpath):
-	# Make tmp folder
-	tmpdir=os.path.join(outpath,'tmp')
-	if not os.path.exists(tmpdir):
-		os.makedirs(tmpdir)		
-	taskname = taskdir.split('/')[-1]
-	umid=taskname.split('_')[2]
-	extracted=[]
-	for i in range(1,13):
-		zipname=os.path.join(taskdir,taskname+'_'+str(i)+'.zip')
-		ncfile=umid+'ga.pe'+file_map[i]+'.nc'
-		if os.path.exists(zipname):
-			zip=zipfile.ZipFile(zipname,'r')
-			# Just extract the monthly means from regional model
-			zip.extract(ncfile,tmpdir)
-			extracted.append(os.path.join(tmpdir,ncfile))
-		# Only return if all of the zips are present
-		else: return []
+		else: raise Exception('Not all zips are present')
 	return extracted
 	
 def create_netcdf(template,data,outname):
@@ -141,6 +108,8 @@ def create_netcdf(template,data,outname):
 if __name__=='__main__':
 	incoming='/gpfs/projects/cpdn/storage/boinc/upload/hadam3p_eu/batch100/hadam3p_eu_*'
 	outpath='/gpfs/projects/cpdn/scratch/cenv0437/batch_100'
+#	incoming='/gpfs/projects/cpdn/storage/boinc/upload/hadam3p_eu/batch43/hadam3p_eu_????_2*'
+#	outpath='/gpfs/projects/cpdn/scratch/cenv0437/batch_43'
 	# Make sure output exists
 	if not os.path.exists(outpath):
 		os.makedirs(outpath)
@@ -148,6 +117,21 @@ if __name__=='__main__':
 	tasks=glob.glob(incoming)
 	for taskdir in tasks:
 		taskname = taskdir.split('/')[-1]
-		ncfiles=extract_zips(taskdir,outpath)
 		outfile=os.path.join(outpath,taskname+'_tasmean.nc')
-		yearly_mean(ncfiles,outfile)
+		print outfile
+		
+		if not os.path.exists(outfile):
+			try:
+				ncfiles=extract_zips(taskdir,outpath)
+				meantemp=yearly_mean(ncfiles)
+				tmp=netcdf_file(ncfiles[0],'r') # use as a template for output file
+				create_netcdf(tmp,meantemp,outfile)
+				# Remove temporary ncfiles
+				for f in ncfiles:
+					os.remove(f)
+			# Copy data to arcus
+#				os.system('scp '+outfile+' cenv0437@arcus.oerc.ox.ac.uk:data/batch_100')
+			except Exception as e:
+				print 'Error for',taskname,e
+		else:
+			print taskname, 'already converted to yearly mean'
