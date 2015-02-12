62ca9cc19599e668ec8696a1b5b335ec54c337c3
Peter Uhe <Peter.Uhe@ouce.ox.ac.uk>
2015-02-12 11:54:19 +0000

    Update

--- wah_eu_2014/calculate_returntimes.py
+++ wah_eu_2014/calculate_returntimes.py
@@ -1,5 +1,5 @@
 import glob,os
-from sort_umids import sort_tasknames
+from sort_umids import sort_tasknames,choose_mask,choose_tasknames
 import numpy as np
 from netcdf_file import netcdf_file
 import matplotlib.pyplot as plt
@@ -122,28 +122,133 @@ def create_netcdf_stripped(template,data,outname,lat_start=4,lat_end=7,lon_start
 	
 	outfile.close()
 
+###############
+
+# Calculate the return time of the mean of the data being greater than an observational dataset
+def ret_time(data,obs):
+
+	ensembles=data.shape[0]*1.0 # First dimension of data is for ensembles. 
+	return ensembles/(data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)
+
+#################
+
+def print_region_returntimes(historical_data,natural_data,clim_hist_data,clim_nat_data,obs,xmin,xmax,ymin,ymax):
+	print_returntimes(historical_data[:,ymin:ymax,xmin:xmax],natural_data[:,ymin:ymax,xmin:xmax],clim_hist_data[:,ymin:ymax,xmin:xmax],clim_nat_data[:,ymin:ymax,xmin:xmax],obs[ymin:ymax,xmin:xmax])
+
+
+def print_returntimes(historical_data,natural_data,clim_hist_data,clim_nat_data,obs):
+
+	ret_hist=ret_time(historical_data,obs)
+	print '2014 All Forcings return time:',ret_hist
+
+# CCSM4	
+	model_mask=choose_mask(natural_files,'z2go','z2m7')+choose_mask(natural_files,'z4so','z5kf')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014, CCSM4:', ret_model
+	
+# Canesm
+	model_mask=choose_mask(natural_files,'z32w','z38f')+choose_mask(natural_files,'z7vs','z8nj')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014, Canesm:', ret_model
+	
+# CNRM-CM5	
+	model_mask=choose_mask(natural_files,'z38g','z3dz')+choose_mask(natural_files,'z8nk','z9fb')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014, CNRM-CM5:', ret_model
+
+	
+# CSIRO-Mk3-6-0
+	model_mask=choose_mask(natural_files,'z3e0','z3jk')+choose_mask(natural_files,'z9fc','za73')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 CSIRO-Mk3-6-0 return time:',ret_model
+	
+# GFDL-CM3
+	model_mask=choose_mask(natural_files,'z2m8','z2rr')+choose_mask(natural_files,'z5kg','z6c7')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 GFDL-CM3 return time:',ret_model
+
+# GISS-E2-H
+	model_mask=choose_mask(natural_files,'z2rs','z2xb')+choose_mask(natural_files,'z6c8','z73z')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 GISS-E2-H return time:',ret_model
+	
+# GISS-E2-R
+	model_mask=choose_mask(natural_files,'z3jk','z3p3')+choose_mask(natural_files,'za74','zayv')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 GISS-E2-R return time:',ret_model
+	
+# HadGEM2-ES
+	model_mask=choose_mask(natural_files,'z2xc','z32v')+choose_mask(natural_files,'z740','z7vr')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 HadGEM2-ES return time:',ret_model
+	
+# HadGEM2-ES
+	model_mask=choose_mask(natural_files,'z2xc','z32v')+choose_mask(natural_files,'z740','z7vr')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 HadGEM2-ES return time:',ret_model
+	
+# IPSL-CM5A-LR
+	model_mask=choose_mask(natural_files,'z3p4','z3un')+choose_mask(natural_files,'zayw','zbqn')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 IPSL-CM5A-LR return time:',ret_model
+	
+# IPSL-CM5A-MR
+	model_mask=choose_mask(natural_files,'z3uo','z407')+choose_mask(natural_files,'zbqo','zcif')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 IPSL-CM5A-MR return time:',ret_model
+	
+# MIROC-ESM
+	model_mask=choose_mask(natural_files,'z408','z45r')+choose_mask(natural_files,'zcig','zda7')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 MIROC-ESM return time:',ret_model
+	
+# MMM
+	model_mask=choose_mask(natural_files,'z45s','z4bb')+choose_mask(natural_files,'zda8','ze1z')
+	print sum(model_mask)
+	ret_model=ret_time(natural_data[model_mask,:,:],obs)
+	print '2014 MMM return time:',ret_model
+
+
+########  All models
+
+	ret_nat=ret_time(natural_data,obs)
+	print '2014 Natural return time:',ret_nat
+
+######## Climatologies
+
+	ret_clim_hist=ret_time(clim_hist_data,obs)
+	print 'Climatology All Forcings return time:',ret_clim_hist	
+
+	ret_clim_nat=ret_time(clim_nat_data,obs)
+	print 'Climatology Natural return time:',ret_clim_nat
+
+
+#########################################################################
+# Main function
+#########################################################################
 
 if __name__=='__main__':
-	infiles='/ouce-home/staff/cenv0437/data/batch_100/hadam3p_eu_*_tasmean.nc'
-	bias_files='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/CRUT4/EU_???_temp-cruts_0.44_mean.nc'
-	bias = ave_bias(bias_files)[:,:-10]
-	(historical_files,natural_files)=sort_tasknames(infiles)
 
-	# Load historical data into single array and bias correct
-	print len(historical_files)
-	hist_raw=np.ma.array(load_ensemble(historical_files))[:,:,:-10]
-	# Apply mask (only need to do this when not bias correcting)
-	for i in range(len(historical_files)):
-	        hist_raw[i,:]=np.ma.masked_where(bias.mask,hist_raw[i,:])
-	historical_data=hist_raw-bias
+
+###############  Model climatological bias
+
+	bias_files='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/CRUT4/EU_???_temp-cruts_0.44_mean.nc'
+	bias = ave_bias(bias_files)[:,:]
 	
-	# Load natural simulation data into single array and bias correct
-	print len(natural_files)
-	natural_raw=np.ma.array(load_ensemble(natural_files))[:,:,:-10]
-	# Apply mask (only need to do this when not bias correcting)
-	for i in range(len(natural_files)):
-	        natural_raw[i,:]=np.ma.masked_where(bias.mask,natural_raw[i,:])
-	natural_data=natural_raw-bias
+	
+################  Observations
 	
 	# CRUTEM observational set
 	obs_N96='/ouce-home/staff/cenv0270/CPDN/Europe_2014/observations/final/CRU_T2m_dec-nov_N96_interp_lsm.nc'
@@ -157,32 +262,73 @@ if __name__=='__main__':
 	# Regrid obs to rotated regional grid
 	rot_template='/ouce-home/staff/cenv0437/data/batch_100/hadam3p_eu_z4ao_2013_1_009238311_0_tasmean.nc'
 #	obs=remap_N96_to_rotated(obs_N96,rot_template)
-	obs=remap_p5deg_to_rotated(obs_p5deg,rot_template)[:,:-10]
+	obs=remap_p5deg_to_rotated(obs_p5deg,rot_template)[:,:]
 
-	oldfiles=glob.glob('/ouce-home/staff/cenv0437/data/batch_43/hadam3p_eu_*_tasmean.nc')
-	print len(oldfiles)
-        old_raw=load_ensemble(oldfiles)[:,:,:-10]
-        old_data=old_raw-bias
+
+#################  Model Data:
+
+	infiles=glob.glob('/ouce-home/staff/cenv0437/data/batch_100/hadam3p_eu_*_tasmean.nc')
+	# Have to choose ensemble members by umid
+	historical_files=choose_tasknames(infiles,'z200','z2gn')+choose_tasknames(infiles,'z4c0','z4sn')
+	natural_files=[x for x in infiles if x not in historical_files]
+
+	clim_hist_files=glob.glob('/ouce-home/staff/cenv0437/data/batch_43/hadam3p_eu_*_tasmean.nc')
+	clim_nat_files=glob.glob('/ouce-home/staff/cenv0437/data/batch_45/hadam3p_eu_*_tasmean.nc')
 
 #################################################
-# Calculate diagnostics	
-	
-	# Count number of model runs which are above the obs
-	condition=historical_data>obs
-	count_hist=condition.sum(0)/(len(historical_files)*1.0)
-	
-	condition=natural_data>obs
-	count_nat=condition.sum(0)/(len(natural_files)*1.0)
-	
-	chist2=(historical_data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(historical_files)*1.0)
-	cnat2=(natural_data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(natural_files)*1.0)
+# Load Data	
 
-	chist3=(hist_raw.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(historical_files)*1.0)
-	cnat3=(natural_raw.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(natural_files)*1.0)
+# Load historical data into single array and bias correct
+	print '2014 historical files:',len(historical_files)
+	hist_raw=np.ma.array(load_ensemble(historical_files))[:,:,:]
+	# Apply mask (only need to do this when not bias correcting)
+	for i in range(len(historical_files)):
+	        hist_raw[i,:]=np.ma.masked_where(bias.mask,hist_raw[i,:])
+	historical_data=hist_raw-bias
+	print 'loaded all forcings 2014 data...'
+	
+	
+# Load natural simulation data into single array and bias correct
+	print '2014 natural files:',len(natural_files)
+	natural_raw=np.ma.array(load_ensemble(natural_files))[:,:,:]
+	# Apply mask (only need to do this when not bias correcting)
+	for i in range(len(natural_files)):
+	        natural_raw[i,:]=np.ma.masked_where(bias.mask,natural_raw[i,:])
+	natural_data=natural_raw-bias
+	print 'loaded natural 2014 data...'
 
-# Old ensemble of historical simulations between 2000-2008
-	cold=(old_data.mean(1).mean(1)>obs.mean(0).mean(0)).sum(0)/(len(oldfiles)*1.0)
+######## Climatologies
 
+# Ensemble of historical simulations between 2000-2012
+	print '1985-2011 all forcings files:',len(clim_hist_files)
+	clim_hist_data=load_ensemble(clim_hist_files)[:,:,:]-bias
+	print 'loaded all forcings 1985-2011 data...'
+
+# Ensemble of natural simulations between 2000-2012
+	print '1985-2011 natural files:',len(clim_nat_files)
+	clim_nat_data=load_ensemble(clim_nat_files)[:,:,:]-bias
+	print 'loaded natural 1985-2011 data...'
+
+########################################################
+# Print Diagnostics
+
+	print '\n######## ALL OF EUROPE #########\n'
+	print_returntimes(historical_data,natural_data,clim_hist_data,clim_nat_data,obs)
+	print '\n######## GERMANY REGION #########\n'
+	print_region_returntimes(historical_data,natural_data,clim_hist_data,clim_nat_data,obs,10,50,50,60)
+
+
+
+########
+
+# Without bias correction
+#	ret_hist_biased=ret_time(hist_raw,obs)
+#	print '2014 All forcings, no bias correction:',ret_hist_biased
+#	ret_nat_biased=ret_time(natural_raw,obs)
+#	print '2014 Natural, no bias correction:',ret_nat_biased
+
+	
+	
 # Region to look at germany:
 	hmean=hist_raw[:,50:60,10:50].mean(1).mean(1)
 	nmean=natural_raw[:,50:60,10:50].mean(1).mean(1)	
@@ -190,26 +336,29 @@ if __name__=='__main__':
 	chist4=(hmean>obsmean).sum(0)/(len(historical_files)*1.0)
 	cnat4=(nmean>obsmean).sum(0)/(len(natural_files)*1.0)
 
-	print 'return times: all forcings=',1/chist2,'natural forcings=',1/cnat2,'2000-2008=',1/cold
-	print 'far',1-(cnat2/chist2)
-	print 'change in risk', (chist2-cnat2)/cnat2
+	print 'return times: all forcings=',chist2,'natural forcings=',cnat2,'2000-2008=',cold
+	print 'far',1-(chist2/cnat2)
+	print 'change in risk', cnat2/chist2
 	
 	print 'Without bias correction:'
 	print 'return times: all forcings=',1/chist3,'natural forcings=',1/cnat3
 	print 'far',1-(cnat3/chist3)
-	print 'change in risk', (chist3-cnat3)/cnat3
+	print 'change in risk', cnat3/chist3
 	
 	print 'Germany:'
 	print 'return times: all forcings=',1/chist4,'natural forcings=',1/cnat4
 	print 'far',1-(cnat4/chist4)
 	print 'change in risk', (chist4-cnat4)/cnat4
 	
-	# Non vectorised way of calculating count 
-#	count = np.zeros([119,122])
-#	for i in range(122):
-#		for j in range(119):
-#			count[j,i] = (historical_data[:,j,i]>obs).sum()/(len(historical_files)*1.0)
+
 	
+	# Spatially resolved count of ensembles hotter than obs:	
+	condition=historical_data>obs
+	count_hist=condition.sum(0)/(len(historical_files)*1.0)
+	
+	condition=natural_data>obs
+	count_nat=condition.sum(0)/(len(natural_files)*1.0)
+
 	# Change in likelihood
 	change=(count_hist-count_nat)/(count_nat+1e-5)
 	
--- wah_eu_2014/sort_umids.py
+++ wah_eu_2014/sort_umids.py
@@ -1,5 +1,5 @@
 import glob,os
-#import numpy as np
+import numpy as np
 #from scipy.io.netcdf import netcdf_file
 #from netcdf_file import netcdf_file
 
@@ -15,25 +15,41 @@ def umid_to_int(umid_string):
 # umid 000z 35
 # umid 0010 36
 # umid 00a0 360
-	id_int=0
-	for i in range(4):
-		c=umid_string[-i-1]
-		if c.isdigit():
-			id_int=id_int+int(c)*(36**i)
-		else:
-			id_int=id_int+(ord(c)-ord('a')+10)*36**i
-	return id_int
+	return int(umid_string,36) # Use inbuilt python function
 
 def sort_tasknames(infiles):
+	#If necessary glob infiles from a string into a list
+	if isinstance(infiles,basestring):infiles=glob.glob(infiles)
 	natural_files=[]
 	historical_files=[]
-	for f in glob.glob(infiles):
+	for f in infiles:
 		umid=os.path.basename(f).split('_')[2]
 		if umid_to_int(umid)>=umid_to_int('z2go'):
 			natural_files.append(f)
 		else:
 			historical_files.append(f)
 	return (historical_files,natural_files)
+
+def choose_tasknames(infiles,startid,endid):
+        choosefiles=[]
+        #If necessary glob infiles from a string into a list
+        if isinstance(infiles,basestring):infiles=glob.glob(infiles)
+        for f in infiles:
+                umid=os.path.basename(f).split('_')[2]
+                if umid_to_int(startid)<=umid_to_int(umid) and umid_to_int(umid)<=umid_to_int(endid):
+                        choosefiles.append(f)
+        return choosefiles
+        
+def choose_mask(infiles,startid,endid):
+	#If necessary glob infiles from a string into a list
+	if isinstance(infiles,basestring):infiles=glob.glob(infiles)
+	choosefiles=np.zeros(len(infiles),dtype=bool)
+	for i,f in enumerate(infiles):
+		umid=os.path.basename(f).split('_')[2]
+		if umid_to_int(startid)<=umid_to_int(umid) and umid_to_int(umid)<=umid_to_int(endid):
+			choosefiles[i]=True
+        return choosefiles
+        
 
 if __name__=='__main__':
 	infiles='/home/ouce-cpdn/cenv0437/data/batch_100/hadam3p_eu_*_tasmean.nc'
